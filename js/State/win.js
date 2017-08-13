419212af3aaf88dde1bebeb9fff374c8cbf65570
jiuis <ltlinh311@gmail.com>
2017-08-13 20:44:23 +0700

    End broken pieces

--- js/State/win.js
+++ js/State/win.js
@@ -11,9 +11,47 @@ var winState = {
     Nakama.game.load.image('background','Assets/background2.png');
     Nakama.game.load.image('homeButton', 'Assets/homeButton.png');
     Nakama.game.load.image('replayButton', 'Assets/replayButton.png');
+    Nakama.game.load.image('broken1', 'Assets/broke1.png');
+    Nakama.game.load.image('broken2', 'Assets/broke2.png');
+    Nakama.game.load.image('broken3', 'Assets/broke3.png');
+    Nakama.game.load.image('broken4', 'Assets/broke4.png');
   },
   create: function(){
     Nakama.game.add.sprite(0, 0, 'background');
+
+    Nakama.shipBroke1 = Nakama.game.add.sprite(300, 100, 'broken1');
+    Nakama.shipBroke1.scale.set(0.5);
+    Nakama.game.physics.arcade.enable(Nakama.shipBroke1);
+    Nakama.shipBroke1.body.velocity.x = 20;
+    Nakama.shipBroke1.body.velocity.y = 40;
+    Nakama.shipBroke1.anchor = new Phaser.Point(0.5, 0.5);
+    // Nakama.shipBroke1.rotation += Nakama.game.math.degToRad(10);
+
+    Nakama.shipBroke2 = Nakama.game.add.sprite(300, 50, 'broken2');
+    Nakama.shipBroke2.scale.set(0.5);
+    Nakama.game.physics.arcade.enable(Nakama.shipBroke2);
+    Nakama.shipBroke2.body.velocity.x = -10;
+    Nakama.shipBroke2.body.velocity.y = 20;
+    Nakama.shipBroke2.anchor = new Phaser.Point(0.5, 0.5);
+    // Nakama.shipBroke2.rotation += Nakama.game.math.degToRad(-10);
+
+
+    Nakama.shipBroke3 = Nakama.game.add.sprite(350, 100, 'broken3');
+    Nakama.shipBroke3.scale.set(0.5);
+    Nakama.game.physics.arcade.enable(Nakama.shipBroke3);
+    Nakama.shipBroke3.body.velocity.x = 50;
+    Nakama.shipBroke3.body.velocity.y = 0;
+    Nakama.shipBroke3.anchor = new Phaser.Point(0.5, 0.5);
+    // Nakama.shipBroke3.rotation += Nakama.game.math.degToRad(5);
+
+
+    Nakama.shipBroke4 = Nakama.game.add.sprite(350, 50, 'broken4');
+    Nakama.shipBroke4.scale.set(0.5);
+    Nakama.game.physics.arcade.enable(Nakama.shipBroke4);
+    Nakama.shipBroke4.body.velocity.x = 10;
+    Nakama.shipBroke4.body.velocity.y = 10;
+    Nakama.shipBroke4.anchor = new Phaser.Point(0.5, 0.5);
+    // Nakama.shipBroke4.rotation += Nakama.game.math.degToRad(-7);
 
     var checkReplay = false;
     var checkReHome = false;
@@ -28,10 +66,18 @@ var winState = {
       this.rehome();
     }, this);
 
+    //broken SHIP
+
     Nakama.style = { font: "bold 50px Arial", fill: "white", boundsAlignH: "center", boundsAlignV: "middle" };
     var score = Nakama.game.add.text(Nakama.game.world.centerX, Nakama.game.world.centerY,
     "     Score: " + Nakama.score + "\nHighscore: " + localStorage.getItem("highscore") , Nakama.style);
     score.anchor.setTo(0.5, 0.5);
+  },
+  update: function() {
+    Nakama.shipBroke1.rotation += Nakama.game.math.degToRad(1);
+    Nakama.shipBroke2.rotation += Nakama.game.math.degToRad(-1);
+    Nakama.shipBroke3.rotation += Nakama.game.math.degToRad(2);
+    Nakama.shipBroke4.rotation += Nakama.game.math.degToRad(-0.5);
   },
   rehome: function(){
     Nakama.game.state.start('menu');
